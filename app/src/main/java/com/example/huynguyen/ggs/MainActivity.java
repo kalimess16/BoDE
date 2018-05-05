e2acf03f26fc9d23ebd645e12864b488494725e4
kalimess16 <kalimess1997@gmail.com>
2018-05-05 20:04:58 +0700

    mergr login

--- app/src/main/java/com/example/huynguyen/ggs/MainActivity.java
+++ app/src/main/java/com/example/huynguyen/ggs/MainActivity.java
@@ -3,31 +3,38 @@ package com.example.huynguyen.ggs;
 
 import android.content.Intent;
 
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
 
     EditText username, password;
     Button bLogin, bRegister, bFP, bGoogle;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AnhXa();
-
+        mAuth = FirebaseAuth.getInstance();
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Login();
             }
         });
@@ -55,14 +63,32 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void Login() {
-     /*   Intent intent = new Intent(MainActivity.this, Voice.class);
-        startActivity(intent);*/
-        if (checkdta() == true) {
-            Dangnhap();
-        } else {
 
-        }
+
+    private void Login() {
+        mAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                           /* Log.d(Tag, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);*/
+                        } else {
+                            // If sign in fails, display a message to the user.
+                         /*   Log.w(Tag, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);*/
+                        }
+
+                        // ...
+                    }
+                });
+        Intent intent = new Intent(MainActivity.this, Voice.class);
+        startActivity(intent);
+
 
     }
 
